fd037b4dcfa41a1e9bca41bea3fab987e8bebbd6
I460632 <i460632@fhict.nl>
2023-06-08 13:21:23 +0200

    change

--- NetworkMirror/main.tf
+++ NetworkMirror/main.tf
@@ -158,11 +158,11 @@ data "template_file" "user_data" {
 resource "aws_instance" "test_instance" {
   ami                         = "ami-053b0d53c279acc90"
   instance_type               = "c4.large"
-  subnet_id                   = aws_subnet.public_subnet.id
+  subnet_id                   = aws_subnet.private_subnet.id
   associate_public_ip_address = true
-  private_ip                  = "10.0.1.21"
+  private_ip                  = "10.0.2.20"
 
-  vpc_security_group_ids = [aws_security_group.public_sg.id]
+  vpc_security_group_ids = [aws_security_group.private_sg.id]
 
   tags = {
     Name = "test"
